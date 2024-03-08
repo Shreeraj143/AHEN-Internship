ef3720e00ebb0aece4979383012f2a37f67faacb
Shreeraj Shinde <shreerajs123@gmail.com>
2024-03-08 17:28:41 +0530

    Added home page and profile page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import home from "../assets/driving-school/home.png";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div
+        className={`cover h-[calc(100vh-52px)]`}
+        style={{
+          backgroundImage: `url(${home})`,
+          objectFit: "cover",
+          backgroundSize: "100% 100%",
+        }}
+      >
+        <div className="py-8 px-12 backdrop-blur-sm h-[calc(100vh-52px)] w-fit">
+          <h1 className="text-5xl font-bold text-center">
+            Drive Like an expert
+            <br />
+            with
+            <br />
+            <span className="text-red-500">Ahen </span>
+            <span className="text-blue-700">Driving </span>
+            <span className="text-teal-500">School </span>
+          </h1>
+          <p>This is a driving school website</p>
+        </div>
+      </div>
+    </div>
+  );
 }
